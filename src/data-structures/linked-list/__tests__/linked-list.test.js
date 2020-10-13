4fa3849cb6bdac12f6055d913b30086f50578a7e
Huy Dang <hdang.log@gmail.com>
2020-10-12 20:48:18 -0700

    Implment append LinkedList

--- src/data-structures/linked-list/__tests__/linked-list.test.js
+++ src/data-structures/linked-list/__tests__/linked-list.test.js
@@ -1,3 +1,54 @@
-it('should true', () => {
-  expect(true).toBeTruthy()
+import LinkedList from '../linked-list'
+
+describe('LinkedList', () => {
+  it.only('should create empty linked list', () => {
+    const linkedList = new LinkedList()
+
+    expect(linkedList.head).toBeNull()
+    expect(linkedList.tail).toBeNull()
+
+    expect(linkedList.toString()).toBe('')
+  })
+
+  it('should append node to linked list', () => {
+    const linkedList = new LinkedList()
+
+    linkedList.append(1)
+    linkedList.append(2)
+
+    expect(linkedList.toString()).toBe('1,2')
+    expect(linkedList.tail.next).toBeNull()
+  })
+
+  it.only('should prepend node to linked list', () => {
+    const linkedList = new LinkedList()
+
+    linkedList.prepend(2)
+
+    expect(linkedList.head.toString()).toBe('2')
+    expect(linkedList.tail.toString()).toBe('2')
+
+    linkedList.prepend(3)
+    linkedList.append(1)
+
+    expect(linkedList.toString()).toBe('3,2,1')
+  })
+
+  it('should delete node by value from linked list', () => {})
+
+  it('should delete linked list tail', () => {})
+
+  it('should delete linked list head', () => {})
+
+  it('should be possible to store objects in the list and to print them out', () => {})
+
+  it('should find node by value', () => {})
+
+  it('should find node by callback', () => {})
+
+  it('should create linked list from array', () => {})
+
+  it('should find node by means of custom compare function', () => {})
+
+  it('should reverse linked list', () => {})
 })
--- src/data-structures/linked-list/linked-list.js
+++ src/data-structures/linked-list/linked-list.js
@@ -1,28 +1,64 @@
 import LinkedListNode from './linked-list-node'
+import Comparator from '../../utils/comparator/comparator'
 
 export default class LinkedList {
-  constuctor() {}
+  /**
+   *
+   * @param {(a:*, b:*) => boolean} comparatorFunction
+   */
+  constructor(comparatorFunction) {
+    /** @var LinkedListNode */
+    this.head = null
+
+    /** @var LinkedListNode */
+    this.tail = null
+
+    this.compare = new Comparator(comparatorFunction)
+  }
 
   /**
    * @param {*} value
    * @returns {LinkedList}
    */
-  prepend(value) {}
+  prepend(value) {
+    // Make new node to be a head.
+    const newNode = new LinkedListNode(value, this.head)
+    this.head = newNode
+
+    // If there is no tail yet let's make new node to be a tail.
+    if (!this.tail) {
+      this.tail = newNode
+    }
+
+    return this
+  }
 
   /**
    * @param {*} value
    * @returns {LinkedList}
    */
-  append(value)
+  append(value) {
+    const newNode = new LinkedListNode(value)
 
+    // If there is no head yet let's make new node a head
+    if (!this.head && !this.tail) {
+      this.head = newNode
+      this.tail = newNode
+      return this
+    }
 
+    // Attach newNode to the end of linked list
+    this.tail.next = newNode
+    this.tail = newNode
 
+    return this
+  }
 
   /**
    * @param {*} value
    * @return {LinkedListNode}
    */
-  delete(value)
+  delete(value) {}
 
   /**
    * @param {Object} findParams
@@ -37,7 +73,7 @@ export default class LinkedList {
    */
   deleteTail() {}
 
-    /**
+  /**
    * @returns {LinkedListNode}
    */
   deleteHead() {}
@@ -50,10 +86,29 @@ export default class LinkedList {
   fromArray(values) {}
 
   /**
+   * @returns LinkedListNode[]
+   */
+  toArray() {
+    const nodes = []
+
+    let currentNode = this.head
+    while (currentNode) {
+      nodes.push(currentNode)
+      currentNode = currentNode.next
+    }
+
+    return nodes
+  }
+
+  /**
    * @param {function} [callback]
    * @returns string
    */
-  toString(callback) {}
+  toString(callback) {
+    return this.toArray()
+      .map(node => node.toString(callback))
+      .toString()
+  }
 
   /**
    * Reverses a linked list.
